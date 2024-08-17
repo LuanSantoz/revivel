5d54bbb9033aa31d50a33164d6405507f1dab00e
LuanSantos <luansantos@outlook.com>
2024-08-17 06:10:34 -0300

    v0.01 test
    create task modal ad

--- v0.01/js/script.js
+++ v0.01/js/script.js
@@ -1,6 +1,6 @@
 // global
 
-
+const taskList = document.querySelector(".task-list");
 
 // menu de botões
 
@@ -9,4 +9,30 @@ const openBtnsMenu = document.querySelector(".open-btns-menu");
 
 openBtnsMenu.addEventListener('click', () => {
   btnsMenu.classList.toggle('active');
+  openBtnsMenu.innerText = btnsMenu.classList[1] == "active" ? "Fechar Menu" : "Abrir Menu";
 })
+
+// modal para criar tarefas
+
+const createTaskModal = document.querySelector(".create-task-modal");
+const openCTModal = document.querySelector(".open-ct-modal");
+const closeCTModal = document.querySelector(".close-ct-modal");
+const showSubTasksContainer = document.getElementsByName("task-type-inp");
+const subTasksModal = document.querySelector(".sub-tasks-container");
+const createTaskBtn = document.querySelector(".create-task-btn");
+
+openCTModal.addEventListener('click', () => {
+  createTaskModal.style.display = "block"
+});
+
+closeCTModal.addEventListener('click', () => {
+  createTaskModal.style.display = "none"
+});
+
+showSubTasksContainer[0].addEventListener('input', () => {
+  subTasksModal.style.display = "none";
+})
+
+showSubTasksContainer[1].addEventListener('input', () => {
+  subTasksModal.style.display = "block";
+});
