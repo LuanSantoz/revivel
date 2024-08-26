54dcd1d4817a02672ef9bf5f946131d457807698
LuanSantos <luansantos@outlook.com>
2024-08-26 06:04:14 -0300

    v0.011
    style correction, add task(incomplet).

--- v0.01/js/script.js
+++ v0.01/js/script.js
@@ -17,22 +17,40 @@ openBtnsMenu.addEventListener('click', () => {
 const createTaskModal = document.querySelector(".create-task-modal");
 const openCTModal = document.querySelector(".open-ct-modal");
 const closeCTModal = document.querySelector(".close-ct-modal");
-const showSubTasksContainer = document.getElementsByName("task-type-inp");
-const subTasksModal = document.querySelector(".sub-tasks-container");
 const createTaskBtn = document.querySelector(".create-task-btn");
 
 openCTModal.addEventListener('click', () => {
-  createTaskModal.style.display = "block"
+  createTaskModal.style.display = "flex"
 });
 
 closeCTModal.addEventListener('click', () => {
   createTaskModal.style.display = "none"
 });
 
-showSubTasksContainer[0].addEventListener('input', () => {
-  subTasksModal.style.display = "none";
-})
+// criar tarefa
 
-showSubTasksContainer[1].addEventListener('input', () => {
-  subTasksModal.style.display = "block";
-});
+createTaskBtn.addEventListener('click', () => {
+  const taskNameInp = document.querySelector(".task-name-inp").value;
+  const taskColorInp = document.querySelector(".task-color-inp").value;
+  const taskTimeInp = document.querySelector("#task-time-inp").value;
+
+  const taskGoalInp = document.querySelector(".task-goal-inp").value;
+  const taskQntInp = document.querySelector("#task-qnt-inp").value;
+  const taskAnotationInp = document.querySelector(".task-anotation-inp");
+
+  const task = document.createElement("div");
+  const taskInfo = document.createElement("p");
+  const taskCheck = document.createElement("span");
+
+  task.classList.add("task");
+
+  taskInfo.classList.add("task-info");
+  taskCheck.classList.add("task-check");
+
+  taskInfo.innerHTML = `<p>${taskNameInp}</p><p>${taskQntInp} ${taskGoalInp}.</p>`;
+  
+
+  task.appendChild(taskInfo);
+  task.appendChild(taskCheck);
+  taskList.appendChild(task);
+})
