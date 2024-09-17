1e50b4c29ef5092db42c9ee07e6f6feafe019352
LuanSantoz <122650656+LuanSantoz@users.noreply.github.com>
2024-09-17 18:47:19 -0300

    Update script.js
    Create task modal interations. Obs:incompleto.

--- js/script.js
+++ js/script.js
@@ -19,6 +19,10 @@ const openCTModal = document.querySelector(".open-ct-modal");
 const closeCTModal = document.querySelector(".close-ct-modal");
 const createTaskBtn = document.querySelector(".create-task-btn");
 
+const taskTypeInp = document.querySelector("#task-type-inp");
+const taskGoalInp = document.querySelector("#task-goal-inp");
+const addSTContainer = document.querySelector(".add-sub-tasks");
+
 openCTModal.addEventListener('click', () => {
   createTaskModal.style.display = "flex"
 });
@@ -27,32 +31,42 @@ closeCTModal.addEventListener('click', () => {
   createTaskModal.style.display = "none"
 });
 
+taskTypeInp.addEventListener("input", () => {
+  if (taskTypeInp.value == "simples") {
+    taskGoalInp.style.display = "none";
+    addSTContainer.style.display = "none";
+  } else if (taskTypeInp.value == "reps" || taskTypeInp.value == "pags" || taskTypeInp.value == "temp") {
+    taskGoalInp.style.display = "block";
+    addSTContainer.style.display = "none";
+  } else if (taskTypeInp.value == "st") {
+    taskGoalInp.style.display = "none";
+    addSTContainer.style.display = "block";
+  }
+});
+
 // criar tarefa
 
 createTaskBtn.addEventListener('click', () => {
   const taskNameInp = document.querySelector(".task-name-inp").value;
   const taskColorInp = document.querySelector(".task-color-inp").value;
   const taskTimeInp = document.querySelector("#task-time-inp").value;
-
-  const taskGoalInp = document.querySelector(".task-goal-inp").value;
-  const taskQntInp = document.querySelector("#task-qnt-inp").value;
   const taskAnotationInp = document.querySelector(".task-anotation-inp");
 
   if (taskNameInp == "") {
     alert("preencha o campo para adicionar um nome a tarefa");
   } else {
     const task = document.createElement("div");
-    const taskInfo = document.createElement("p");
+    const taskInfo = document.createElement("div");
+    const taskName = document.createElement("p");
+    const taskGoal = document.createElement("p");
     const taskCheck = document.createElement("span");
 
     task.classList.add("task");
-
     taskInfo.classList.add("task-info");
     taskCheck.classList.add("task-check");
 
-    taskInfo.innerHTML = `<p>${taskNameInp}</p><p>${taskQntInp} ${taskGoalInp}.</p>`;
-    taskInfo.firstElementChild.id = "task-name";
-    
+    taskInfo.appendChild(taskName);
+    taskInfo.appendChild(taskGoal);
 
     task.appendChild(taskInfo);
     task.appendChild(taskCheck);
@@ -62,6 +76,7 @@ createTaskBtn.addEventListener('click', () => {
 
 /*
   mostrar elementos escondidos de acordo com o valor de task-type
+  
   criar modelo de hábito
     estrutura - nome, check, botão abrir menu de botões.
       esc: meta/sub-tarefas, menu de botões(editar, apagar)
